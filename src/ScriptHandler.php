bd9b3ce5bc87286afc84d3da6488ad4835eb90b1
Joby Elliott <joby@byjoby.com>
2019-07-13 11:32:15 -0600

    update/install handler stubs

---
+++ src/ScriptHandler.php
@@ -0,0 +1,13 @@
+<?php
+/* digraph-project-core | https://gitlab.com/byjoby/digraph-project-core | MIT License */
+namespace DigraphProject;
+
+class ScriptHandler
+{
+    public static function updateHandler($event)
+    {
+    }
+    public static function installHandler($event)
+    {
+    }
+}
